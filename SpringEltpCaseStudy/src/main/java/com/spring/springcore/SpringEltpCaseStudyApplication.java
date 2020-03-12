1b2440da7eaed205742e3a0384397664432d8a00
NEW <NEW@DESKTOP-PJKM185.local>
2020-03-12 16:06:17 +0800

    First Commit.

---
+++ SpringEltpCaseStudy/src/main/java/com/spring/springcore/SpringEltpCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEltpCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEltpCaseStudyApplication.class, args);
+	}
+
+}
