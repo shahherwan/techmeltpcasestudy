79e8a2d250bdf68a86039409dd6e486409e47248
Nazreen Misrawi <Nazreen Misrawi@LAPTOP-08OKSGQR>
2020-03-13 18:55:29 +0800

    Changed password of both employees BCrypt's encrypted result of admin.

--- SpringEltpCaseStudy/src/main/resources/data.sql
+++ SpringEltpCaseStudy/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO 
     EMPLOYEES (first_name, last_name, email, role, username, password) 
 VALUES
-      ('Sree', 'Krishna','jsk@gmail.com', 'admin', 'admin', 'admin'),
-      ('Sriman', 'Narayana', 'smn@email.com', 'non-admin', 'employee', 'employee');
+      ('Sree', 'Krishna','jsk@gmail.com', 'admin', 'admin', '$2a$04$RFO8WF54GrqTchno/FSppOcEXtV0FIng9YeYKuK4eWybi18KLf0NC'),
+      ('Sriman', 'Narayana', 'smn@email.com', 'non-admin', 'employee', '$2a$04$RFO8WF54GrqTchno/FSppOcEXtV0FIng9YeYKuK4eWybi18KLf0NC');
